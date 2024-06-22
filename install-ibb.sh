cfe95e32d6254fea1b5db47c627a9bcebac08617
Andrew Duss <=>
2024-06-22 14:00:30 -0600

    fix: move ktunnel install into padi
    Signed-off-by: Andrew Duss <=>

--- install-ibb.sh
+++ install-ibb.sh
@@ -240,103 +240,103 @@ install_k3s () {
   chmod 600 $HOME/.kube/config
 }
 
-install_ktunnel () {
-  if [ "$INSTALL_KTUNNEL" != true ]; then
-    log_info "Install KTunnel flag is not true. Skipping KTunnel installation..."
-    return 1
-  fi
+# install_ktunnel () {
+#   if [ "$INSTALL_KTUNNEL" != true ]; then
+#     log_info "Install KTunnel flag is not true. Skipping KTunnel installation..."
+#     return 1
+#   fi
 
-  if [ ! -f "$KTUNNEL_KUBECONFIG_SECRET_MANIFEST" ]; then
-    log_info "$KTUNNEL_KUBECONFIG_SECRET_MANIFEST"
-    log_fail "KTunnel kubeconfig secret manifest file not found. Cannot Continue"
-  fi
+#   if [ ! -f "$KTUNNEL_KUBECONFIG_SECRET_MANIFEST" ]; then
+#     log_info "$KTUNNEL_KUBECONFIG_SECRET_MANIFEST"
+#     log_fail "KTunnel kubeconfig secret manifest file not found. Cannot Continue"
+#   fi
 
-  if [ ! -d "$IBB_INSTALL_DIR/ktunnel" ]; then
-    mkdir "$IBB_INSTALL_DIR/ktunnel"
-  fi
+#   if [ ! -d "$IBB_INSTALL_DIR/ktunnel" ]; then
+#     mkdir "$IBB_INSTALL_DIR/ktunnel"
+#   fi
 
-  if [ ! -f "$IBB_INSTALL_DIR/ktunnel/ca.conf" ]; then
-    log_info "Writing ktunnel ca.conf"
-    cat << EOF > "$IBB_INSTALL_DIR/ktunnel/ca.conf"
-[req]
-req_extensions      = v3_req
-distinguished_name  = req_distinguished_name
-[ v3_req ]
-basicConstraints  = CA:TRUE
-keyUsage          = nonRepudiation, digitalSignature, keyEncipherment
-extendedKeyUsage  = serverAuth
-[req_distinguished_name]
-countryName                 = Country Name (2 letter code)
-countryName_default         = US
-stateOrProvinceName         = State or Province Name (full name)
-stateOrProvinceName_default = Colorado
-localityName                = Locality Name (eg, city)
-localityName_default        = Denver
-organizationName            = Organization Name (eg, company)
-organizationName_default    = IBB
-commonName                  = Common Name (eg, YOUR name)
-commonName_default          = ibb-ktunnel-sidecar-injector
-commonName_max              = 64
-emailAddress                = Email Address
-emailAddress_default        = admin@ibbproject.com
-EOF
-  fi
+#   if [ ! -f "$IBB_INSTALL_DIR/ktunnel/ca.conf" ]; then
+#     log_info "Writing ktunnel ca.conf"
+#     cat << EOF > "$IBB_INSTALL_DIR/ktunnel/ca.conf"
+# [req]
+# req_extensions      = v3_req
+# distinguished_name  = req_distinguished_name
+# [ v3_req ]
+# basicConstraints  = CA:TRUE
+# keyUsage          = nonRepudiation, digitalSignature, keyEncipherment
+# extendedKeyUsage  = serverAuth
+# [req_distinguished_name]
+# countryName                 = Country Name (2 letter code)
+# countryName_default         = US
+# stateOrProvinceName         = State or Province Name (full name)
+# stateOrProvinceName_default = Colorado
+# localityName                = Locality Name (eg, city)
+# localityName_default        = Denver
+# organizationName            = Organization Name (eg, company)
+# organizationName_default    = IBB
+# commonName                  = Common Name (eg, YOUR name)
+# commonName_default          = ibb-ktunnel-sidecar-injector
+# commonName_max              = 64
+# emailAddress                = Email Address
+# emailAddress_default        = admin@ibbproject.com
+# EOF
+#   fi
 
-  if [ ! -f "$IBB_INSTALL_DIR/ktunnel/csr.conf" ]; then
-    log_info "Writing ktunnel csr.conf"
-    cat << EOF > "$IBB_INSTALL_DIR/ktunnel/csr.conf"
-[ req ]
-default_bits        = 2048
-default_keyfile     = sidecar-injector.key
-distinguished_name  = req_distinguished_name
-req_extensions      = req_ext # The extentions to add to the self signed cert
+#   if [ ! -f "$IBB_INSTALL_DIR/ktunnel/csr.conf" ]; then
+#     log_info "Writing ktunnel csr.conf"
+#     cat << EOF > "$IBB_INSTALL_DIR/ktunnel/csr.conf"
+# [ req ]
+# default_bits        = 2048
+# default_keyfile     = sidecar-injector.key
+# distinguished_name  = req_distinguished_name
+# req_extensions      = req_ext # The extentions to add to the self signed cert
  
-[ req_distinguished_name ]
-countryName                 = Country Name (2 letter code)
-countryName_default         = US
-stateOrProvinceName         = State or Province Name (full name)
-stateOrProvinceName_default = New York
-localityName                = Locality Name (eg, city)
-localityName_default        = NYC
-organizationName            = Organization Name (eg, company)
-organizationName_default    = IBB
-commonName                  = Common Name (eg, YOUR name)
-commonName_default          = ibb-ktunnel-sidecar-injector
-commonName_max              = 64
+# [ req_distinguished_name ]
+# countryName                 = Country Name (2 letter code)
+# countryName_default         = US
+# stateOrProvinceName         = State or Province Name (full name)
+# stateOrProvinceName_default = New York
+# localityName                = Locality Name (eg, city)
+# localityName_default        = NYC
+# organizationName            = Organization Name (eg, company)
+# organizationName_default    = IBB
+# commonName                  = Common Name (eg, YOUR name)
+# commonName_default          = ibb-ktunnel-sidecar-injector
+# commonName_max              = 64
  
-[ req_ext ]
-subjectAltName          = @alt_names
+# [ req_ext ]
+# subjectAltName          = @alt_names
 
-[alt_names]
-DNS.1   = ibb-ktunnel-sidecar-injector
-DNS.2   = ibb-ktunnel-sidecar-injector.kube-system
-DNS.3   = ibb-ktunnel-sidecar-injector.kube-system.svc
-EOF
-  fi
+# [alt_names]
+# DNS.1   = ibb-ktunnel-sidecar-injector
+# DNS.2   = ibb-ktunnel-sidecar-injector.kube-system
+# DNS.3   = ibb-ktunnel-sidecar-injector.kube-system.svc
+# EOF
+#   fi
 
-  # Generate OpenSSL Certificates needed
-  log_info "Generating ktunnel certificates and keys"
-  log_info "Creating ca.key"
-  openssl genrsa -out $IBB_INSTALL_DIR/ktunnel/ca.key 4096 | tee -a $IBB_LOG_FILE
-  log_info "Creating ca.crt"
-  openssl req -x509 -config $IBB_INSTALL_DIR/ktunnel/ca.conf -new -nodes -key $IBB_INSTALL_DIR/ktunnel/ca.key -sha256 -days 999999 -out $IBB_INSTALL_DIR/ktunnel/ca.crt | tee -a $IBB_LOG_FILE
-  log_info "Creating sidecar-injector.key"
-  openssl genrsa -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.key 2048 | tee -a $IBB_LOG_FILE
-  log_info "Creating sidecar-injector.csr"
-  openssl req -new -key $IBB_INSTALL_DIR/ktunnel/sidecar-injector.key -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.csr -config $IBB_INSTALL_DIR/ktunnel/ca.conf | tee -a $IBB_LOG_FILE
-  log_info "Creating the certificate"
-  openssl x509 -req -in $IBB_INSTALL_DIR/ktunnel/sidecar-injector.csr -CA $IBB_INSTALL_DIR/ktunnel/ca.crt -CAkey $IBB_INSTALL_DIR/ktunnel/ca.key -CAcreateserial -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.crt -days 999999 -sha256 -extensions req_ext -extfile $IBB_INSTALL_DIR/ktunnel/csr.conf | tee -a $IBB_LOG_FILE
+#   # Generate OpenSSL Certificates needed
+#   log_info "Generating ktunnel certificates and keys"
+#   log_info "Creating ca.key"
+#   openssl genrsa -out $IBB_INSTALL_DIR/ktunnel/ca.key 4096 | tee -a $IBB_LOG_FILE
+#   log_info "Creating ca.crt"
+#   openssl req -x509 -config $IBB_INSTALL_DIR/ktunnel/ca.conf -new -nodes -key $IBB_INSTALL_DIR/ktunnel/ca.key -sha256 -days 999999 -out $IBB_INSTALL_DIR/ktunnel/ca.crt | tee -a $IBB_LOG_FILE
+#   log_info "Creating sidecar-injector.key"
+#   openssl genrsa -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.key 2048 | tee -a $IBB_LOG_FILE
+#   log_info "Creating sidecar-injector.csr"
+#   openssl req -new -key $IBB_INSTALL_DIR/ktunnel/sidecar-injector.key -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.csr -config $IBB_INSTALL_DIR/ktunnel/ca.conf | tee -a $IBB_LOG_FILE
+#   log_info "Creating the certificate"
+#   openssl x509 -req -in $IBB_INSTALL_DIR/ktunnel/sidecar-injector.csr -CA $IBB_INSTALL_DIR/ktunnel/ca.crt -CAkey $IBB_INSTALL_DIR/ktunnel/ca.key -CAcreateserial -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.crt -days 999999 -sha256 -extensions req_ext -extfile $IBB_INSTALL_DIR/ktunnel/csr.conf | tee -a $IBB_LOG_FILE
 
-  log_info "Adding IBB Project Helm repository"
-  helm repo add ibb https://ibbproject.github.io/helm-charts/ > /dev/null
-  log_info "Updating Helm repositories"
-  helm repo update > /dev/null
-  log_info "Installing ktunnel sidecar injector"
-  helm upgrade --install ibb-ktunnel-inejctor ibb/ibb-ktunnel-injector --namespace kube-system --set-file caCrt=$IBB_INSTALL_DIR/ktunnel/ca.crt,sidecarInjectorCrt=$IBB_INSTALL_DIR/ktunnel/sidecar-injector.crt,sidecarInjectorKey=$IBB_INSTALL_DIR/ktunnel/sidecar-injector.key --wait >> $IBB_LOG_FILE 2>> $IBB_LOG_FILE
+#   log_info "Adding IBB Project Helm repository"
+#   helm repo add ibb https://ibbproject.github.io/helm-charts/ > /dev/null
+#   log_info "Updating Helm repositories"
+#   helm repo update > /dev/null
+#   log_info "Installing ktunnel sidecar injector"
+#   helm upgrade --install ibb-ktunnel-inejctor ibb/ibb-ktunnel-injector --namespace kube-system --set-file caCrt=$IBB_INSTALL_DIR/ktunnel/ca.crt,sidecarInjectorCrt=$IBB_INSTALL_DIR/ktunnel/sidecar-injector.crt,sidecarInjectorKey=$IBB_INSTALL_DIR/ktunnel/sidecar-injector.key --wait >> $IBB_LOG_FILE 2>> $IBB_LOG_FILE
 
-  log_info "Adding the KTunnel kubeconfig"
-  k3s kubectl apply -f $KTUNNEL_KUBECONFIG_SECRET_MANIFEST | tee -a $IBB_LOG_FILE
-}
+#   log_info "Adding the KTunnel kubeconfig"
+#   k3s kubectl apply -f $KTUNNEL_KUBECONFIG_SECRET_MANIFEST | tee -a $IBB_LOG_FILE
+# }
 
 # link_ibb_to_padi() {
 #   if [ "$LINK_TO_PADI" != true ]; then 
@@ -504,7 +504,7 @@ install_helm
 
 # Link must be done before CNS-Dapr or CNS-Kube can be installed
 # link_ibb_to_padi  # --> Installed by `link-padi.sh`
-install_ktunnel
+# install_ktunnel
 install_dapr
 
 # install_cns_dapr  # --> Installed by `link-padi.sh`
--- link-padi.sh
+++ link-padi.sh
@@ -12,7 +12,7 @@ IBB_INSTALL_DIR="/opt/ibb"
 IBB_LOG_PATH="$IBB_INSTALL_DIR/logs"
 IBB_LOG_FILE="$IBB_LOG_PATH/install.log"
 IBB_DOWNLOAD_PATH="$IBB_INSTALL_DIR/downloads"
-REQUIRED_BINARIES="curl cut git grep openssl tr" # FORMAT: "curl wget vim otherbinary"
+REQUIRED_BINARIES="curl cut git grep k3s openssl tr" # FORMAT: "curl wget vim otherbinary"
 K3S_INSTALL_SCRIPT_FILENAME="ibb-install-k3s.sh"
 HELM_INSTALL_SCRIPT_FILENAME="ibb-install-helm.sh"
 ARGOCD_NS="argocd"
@@ -125,6 +125,104 @@ install_cns_kube () {
   log_info "CNS Kube Installed"
 }
 
+install_ktunnel () {
+  if [ "$INSTALL_KTUNNEL" != true ]; then
+    log_info "Install KTunnel flag is not true. Skipping KTunnel installation..."
+    return 1
+  fi
+
+  if [ ! -f "$KTUNNEL_KUBECONFIG_SECRET_MANIFEST" ]; then
+    log_info "$KTUNNEL_KUBECONFIG_SECRET_MANIFEST"
+    log_fail "KTunnel kubeconfig secret manifest file not found. Cannot Continue"
+  fi
+
+  if [ ! -d "$IBB_INSTALL_DIR/ktunnel" ]; then
+    mkdir "$IBB_INSTALL_DIR/ktunnel"
+  fi
+
+  if [ ! -f "$IBB_INSTALL_DIR/ktunnel/ca.conf" ]; then
+    log_info "Writing ktunnel ca.conf"
+    cat << EOF > "$IBB_INSTALL_DIR/ktunnel/ca.conf"
+[req]
+req_extensions      = v3_req
+distinguished_name  = req_distinguished_name
+[ v3_req ]
+basicConstraints  = CA:TRUE
+keyUsage          = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage  = serverAuth
+[req_distinguished_name]
+countryName                 = Country Name (2 letter code)
+countryName_default         = US
+stateOrProvinceName         = State or Province Name (full name)
+stateOrProvinceName_default = Colorado
+localityName                = Locality Name (eg, city)
+localityName_default        = Denver
+organizationName            = Organization Name (eg, company)
+organizationName_default    = IBB
+commonName                  = Common Name (eg, YOUR name)
+commonName_default          = ibb-ktunnel-sidecar-injector
+commonName_max              = 64
+emailAddress                = Email Address
+emailAddress_default        = admin@ibbproject.com
+EOF
+  fi
+
+  if [ ! -f "$IBB_INSTALL_DIR/ktunnel/csr.conf" ]; then
+    log_info "Writing ktunnel csr.conf"
+    cat << EOF > "$IBB_INSTALL_DIR/ktunnel/csr.conf"
+[ req ]
+default_bits        = 2048
+default_keyfile     = sidecar-injector.key
+distinguished_name  = req_distinguished_name
+req_extensions      = req_ext # The extentions to add to the self signed cert
+ 
+[ req_distinguished_name ]
+countryName                 = Country Name (2 letter code)
+countryName_default         = US
+stateOrProvinceName         = State or Province Name (full name)
+stateOrProvinceName_default = New York
+localityName                = Locality Name (eg, city)
+localityName_default        = NYC
+organizationName            = Organization Name (eg, company)
+organizationName_default    = IBB
+commonName                  = Common Name (eg, YOUR name)
+commonName_default          = ibb-ktunnel-sidecar-injector
+commonName_max              = 64
+ 
+[ req_ext ]
+subjectAltName          = @alt_names
+
+[alt_names]
+DNS.1   = ibb-ktunnel-sidecar-injector
+DNS.2   = ibb-ktunnel-sidecar-injector.kube-system
+DNS.3   = ibb-ktunnel-sidecar-injector.kube-system.svc
+EOF
+  fi
+
+  # Generate OpenSSL Certificates needed
+  log_info "Generating ktunnel certificates and keys"
+  log_info "Creating ca.key"
+  openssl genrsa -out $IBB_INSTALL_DIR/ktunnel/ca.key 4096 | tee -a $IBB_LOG_FILE
+  log_info "Creating ca.crt"
+  openssl req -x509 -config $IBB_INSTALL_DIR/ktunnel/ca.conf -new -nodes -key $IBB_INSTALL_DIR/ktunnel/ca.key -sha256 -days 999999 -out $IBB_INSTALL_DIR/ktunnel/ca.crt | tee -a $IBB_LOG_FILE
+  log_info "Creating sidecar-injector.key"
+  openssl genrsa -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.key 2048 | tee -a $IBB_LOG_FILE
+  log_info "Creating sidecar-injector.csr"
+  openssl req -new -key $IBB_INSTALL_DIR/ktunnel/sidecar-injector.key -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.csr -config $IBB_INSTALL_DIR/ktunnel/ca.conf | tee -a $IBB_LOG_FILE
+  log_info "Creating the certificate"
+  openssl x509 -req -in $IBB_INSTALL_DIR/ktunnel/sidecar-injector.csr -CA $IBB_INSTALL_DIR/ktunnel/ca.crt -CAkey $IBB_INSTALL_DIR/ktunnel/ca.key -CAcreateserial -out $IBB_INSTALL_DIR/ktunnel/sidecar-injector.crt -days 999999 -sha256 -extensions req_ext -extfile $IBB_INSTALL_DIR/ktunnel/csr.conf | tee -a $IBB_LOG_FILE
+
+  log_info "Adding IBB Project Helm repository"
+  helm repo add ibb https://ibbproject.github.io/helm-charts/ > /dev/null
+  log_info "Updating Helm repositories"
+  helm repo update > /dev/null
+  log_info "Installing ktunnel sidecar injector"
+  helm upgrade --install ibb-ktunnel-inejctor ibb/ibb-ktunnel-injector --namespace kube-system --set-file caCrt=$IBB_INSTALL_DIR/ktunnel/ca.crt,sidecarInjectorCrt=$IBB_INSTALL_DIR/ktunnel/sidecar-injector.crt,sidecarInjectorKey=$IBB_INSTALL_DIR/ktunnel/sidecar-injector.key --wait >> $IBB_LOG_FILE 2>> $IBB_LOG_FILE
+
+  log_info "Adding the KTunnel kubeconfig"
+  k3s kubectl apply -f $KTUNNEL_KUBECONFIG_SECRET_MANIFEST | tee -a $IBB_LOG_FILE
+}
+
 link_ibb_to_padi() {
   if [ "$LINK_TO_PADI" != true ]; then 
     log_info "Link to Padi flag is not true. Skipping..."
@@ -202,8 +300,6 @@ notify_complete () {
 }
 
 # Start the script
-
-# Start the script
 while [[ $# -gt 0 ]]; do
   case $1 in
     --install-dir)
@@ -262,6 +358,7 @@ done
 check_root
 check_required_binaries
 
+install_ktunnel
 # Link must be done before CNS-Dapr or CNS-Kube can be installed
 link_ibb_to_padi
 install_cns_dapr
