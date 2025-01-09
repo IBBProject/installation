bd5a061d02121a439a2907d1571428de20156ae5
Andrew <mindovermiles262@gmail.com>
2025-01-08 20:40:37 -0700

    fix: bump veersion
    Signed-off-by: Andrew <mindovermiles262@gmail.com>

--- install-ibb.sh
+++ install-ibb.sh
@@ -12,7 +12,7 @@ set -o pipefail
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-INSTALL_SCRIPT_VERSION="2.2.0"
+INSTALL_SCRIPT_VERSION="2.2.1"
 
 # Must be a k3s-io tagged release: https://github.com/k3s-io/k3s/releases
 K3S_VERSION="v1.25.16+k3s4"
